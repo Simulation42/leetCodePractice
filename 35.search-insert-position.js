fe84953236e22986830a63ecabf42f425c91eed0
Simulation42 <67242768+Simulation42@users.noreply.github.com>
2021-07-12 19:36:09 -0400

    35.search-insert-position

---
+++ 35.search-insert-position.js
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=35 lang=javascript
+ *
+ * [35] Search Insert Position
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    for (var i = 0; i < nums.length; i++) {
+        if (target <= nums[i]) {
+            return i;
+        }
+    }
+    return nums.length;
+};
+// @lc code=end
+
